c68020f9175aaf7341d3a7b55ea54656ce3d5387
lukebricknell <lukecbricknell@gmail.com>
2022-12-16 20:29:11 +0200

    Final Touch-ups, Adding Team Sponsors

--- public/forms.js
+++ public/forms.js
@@ -34,3 +34,45 @@ const generateSignature = (data, passPhrase = null) => {
 
   return crypto.createHash("md5").update(getString).digest("hex");
 };
+
+const myData = {};
+// Merchant details
+myData["merchant_id"] = "17666473";
+myData["merchant_key"] = "qhioe2p0spns9";
+myData["return_url"] = "http://www.yourdomain.co.za/return_url";
+myData["cancel_url"] = "http://www.yourdomain.co.za/cancel_url";
+myData["notify_url"] = "http://www.yourdomain.co.za/notify_url";
+// Transaction details
+
+// myData["amount"] = donationAmt.value;
+// myData["item_name"] = "Donation";
+// myData["name_first"] = firstName.value;
+// myData["email_address"] = email.value;
+
+myData["amount"] = "100";
+myData["item_name"] = "Donation";
+// Buyer details
+myData["name_first"] = "Test";
+myData["email_address"] = "Test@adopt.com";
+
+// Generate signature
+const myPassphrase = "Ad0p/aQr1_3r./";
+myData["signature"] = generateSignature(myData, myPassphrase);
+document.getElementById("signature").value = myData["signature"];
+
+let htmlForm = `<form action="https://payfast.co.za/eng/process" method="post">`;
+for (let key in myData) {
+  if (myData.hasOwnProperty(key)) {
+    value = myData[key];
+    if (value !== "") {
+      htmlForm += `<input name="${key}" type="hidden" value="${value.trim()}" />`;
+    }
+  }
+}
+
+htmlForm += '<input type="submit" value="Pay Now" /></form>';
+
+const result = fetch("https://payfast.co.za​/eng/process", {
+  method: "POST",
+  body: JSON.stringify(myData),
+}).then((res) => console.log(res));
